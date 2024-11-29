95c1e60aa953f43afdc74b2e76920fa84342f9b3
AbdulBaseer <160875997+AbdulBaseer1939@users.noreply.github.com>
2024-11-29 10:29:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// Problem#1: Declare 3 variables in one statement. 
+var firstVariable = 20, secondVariable = 50, thirdVariable = 50;
+console.log("firstVariable ", firstVariable, " \nsecond variable ", secondVariable, " \nthird variable ", thirdVariable );
