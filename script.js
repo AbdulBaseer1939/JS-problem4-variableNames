09c42c42506286a7f2bb5c84fb4531b1a58b0a66
AbdulBaseer <160875997+AbdulBaseer1939@users.noreply.github.com>
2024-11-29 10:46:52 +0500

    Update script-3problemsDone.js

--- script.js
+++ script.js
@@ -17,3 +17,10 @@ my-variable= "John Doe";//Contains a hyphen, which is not allowed in most langua
 class = "John Doe"; //This is a reserved keyword in many languages.
 my variable = 10;//Contains a space, which is not allowed.
 %discount = 10;//Starts with a special character, which is generally not allowed.
+
+// problem#3: Display this in your browser a)  A heading stating “Rules for naming JS variables”, b)  Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable, c) Variables must begin with a ______, ______ or _____. For example $name, _name or name, d) Variable names are case _________, e) Variable names should not be JS _________.
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>underscores( _ )</b> and <b>dollar signs ($)</b>. For example: $my_1stVariable</p>");
+document.write("<p>Variables must begin with a <b>letter</b>, <b>underscore( _ )</b> or <b>dollar sign ($)</b>. For example: $name, _name or name</p>");
+document.write("<p>Variable names are case <b>sensitive.</b></p>");
+document.write("<p>Variable names should not be JS <b>keywords.</b></p>");
